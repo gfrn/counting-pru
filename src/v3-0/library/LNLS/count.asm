96d5b44f074134e2dc0bd00a19b0ef64443fa067
gfrn <guifreitas89@hotmail.com>
2021-10-01 07:49:56 -0300

    Removes unnecessary defines

--- src/v3-0/library/LNLS/count.asm
+++ src/v3-0/library/LNLS/count.asm
@@ -2,19 +2,15 @@
 
 	.define r16, 		COUNT1
 	.define r30.t0,		OUT1	; P8_45
-	.define r31.t5, 	IN1		; P8_42
 
 	.define r17, 		COUNT2
 	.define r30.t1,		OUT2	; P8_46
-	.define r31.t4, 	IN2		; P8_41
 
 	.define r18, 		COUNT3
 	.define r30.t3,		OUT3	; P8_44
-	.define r31.t7, 	IN3		; P8_40
 
 	.define r19, 	    COUNT4
 	.define r30.t2,	    OUT4   	; P8_43
-	.define r31.t6, 	IN4		; P8_39
 	.global asm_count
 
 asm_count:
@@ -24,24 +20,24 @@ asm_count:
 	ZERO		&COUNT4,4
 
 count1:
-	QBBC	count2, r31, 5
+	QBBC	count2, r31, 5		; P8_42
 	ADD		COUNT1, COUNT1, 1
 	CLR		OUT1
 
 count2:
-	QBBC	count3, r31, 4
+	QBBC	count3, r31, 4		; P8_41
 	ADD		COUNT2, COUNT2, 1
 	CLR		OUT2
 
 count3:
 	LDI		r30, 0x03
-	QBBC	count4, r31, 7
+	QBBC	count4, r31, 7		; P8_40
 	ADD		COUNT3, COUNT3, 1
 	CLR		OUT3
 
 count4:
 	SET		OUT3
-	QBBC	ret_loop, r31, 6
+	QBBC	ret_loop, r31, 6	; P8_39
 	ADD		COUNT4, COUNT4, 1
 	CLR		OUT4
 ret_loop:
